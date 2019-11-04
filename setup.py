44ca50786f330d9939501f1384272f44f26178fd
ysBach <dbstn95@gmail.com>
2019-11-04 11:13:25 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+"""
+"""
+
+from setuptools import setup, find_packages
+
+setup_requires = []
+install_requires = ['numpy',
+                    'pandas',
+                    'astropy >= 2.0',
+                    'ccdproc >= 1.3']
+
+classifiers = ["Intended Audience :: Science/Research",
+               "Operating System :: OS Independent",
+               "Programming Language :: Python :: 3.6"]
+
+setup(
+    name="nicpolpy",
+    version="0.1.dev",
+    author="Yoonsoo P. Bach",
+    author_email="dbstn95@gmail.com",
+    description="",
+    license="",
+    keywords="",
+    url="",
+    classifiers=classifiers,
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=install_requires)
