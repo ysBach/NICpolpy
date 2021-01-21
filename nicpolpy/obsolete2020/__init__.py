2fc25ed2066e1fb6b9f93ff05beae8d82667b4cb
ysBach <dbstn95@gmail.com>
2021-01-21 14:16:39 +0900

    move outdated objects to obsolete2020 directory

---
+++ nicpolpy/obsolete2020/__init__.py
@@ -0,0 +1,8 @@
+'''
+A module to archive the obsolete version of NICPolpy during the development phase in 2020.
+Codes written prior to 2021-01-01 must use functions defined here (likely have the same names in
+modules like ..util, ..preproc, etc) to reproduce the original results.
+'''
+
+from .nicccd import *
+from .preproc import *
